7f9a6835cbffb90dd08b8bebb76a29309d7f03ba
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-05-21 15:04:11 +0500

    update menu result-Modifier

--- local/templates/smart-module-new/components/bitrix/menu/header-fixed-catalog/result_modifier.php
+++ local/templates/smart-module-new/components/bitrix/menu/header-fixed-catalog/result_modifier.php
@@ -1,5 +1,22 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <?
+// -----ПЕРЕНОС-------
+// фильтр по городам, если в разделе все твоары исключены по свойству - этот раздел не выводим
+$iden = explode(".", $_SERVER['HTTP_HOST']); 
+$city_code = $iden[0]; // получаем код города
+if(!empty($city_code)) // и если он есть
+{
+	$res = CIBlockElement::GetList(Array(), Array("IBLOCK_ID"=>4, "PROPERTY_WF_SUBDOMAIN"=>$city_code), false, Array(), Array("ID"));
+	if($ob = $res->Fetch())
+	{
+		global $arrFilter, $_SESSION;
+		$arrFilter = Array(
+			array("!ID" => \CIBlockElement::SubQuery("ID", array("IBLOCK_ID" => 1, "PROPERTY_CITY" => $ob['ID'])))
+		);
+		$_SESSION['CITY']['ID'] = $ob['ID'];
+	}
+}
+// -----ПЕРЕНОС-----
 
 if (!empty($arResult)) {
     // Компонент только для каталога
