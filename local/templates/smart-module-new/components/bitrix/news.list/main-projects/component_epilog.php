6401883d779180642073f8848a21d6e717f11aae
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-05-30 15:50:59 +0300

    fix: center main-projects

--- local/templates/smart-module-new/components/bitrix/news.list/main-projects/component_epilog.php
+++ local/templates/smart-module-new/components/bitrix/news.list/main-projects/component_epilog.php
@@ -1,11 +1,12 @@
+<? CModule::IncludeModule('webfly.seocities');?>
 <script>
     let center = [56.79757896870354, 50.22890452343749]
     let zoom = 5
-    <? if ($_SERVER['SERVER_NAME'] === 'smart-module.ru') : ?>
+    <? if (\CWebflyCities::GetSubDomain() === 'default') : ?>
         center = [59.939099, 30.315877]
         zoom = 9
     <? endif; ?>
-    <? if ($_SERVER['SERVER_NAME'] === 'moskva.smart-module.ru') : ?>
+    <? if (\CWebflyCities::GetSubDomain() === 'moskva') : ?>
         center = [55.755819, 37.617644]
         zoom = 9
     <? endif; ?>
